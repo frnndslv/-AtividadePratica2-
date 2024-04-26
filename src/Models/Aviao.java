6131bd98db6bbcd1746a71ee65ef38dc38e32bcc
mafioletti98 <141834882+mafioletti98@users.noreply.github.com>
2024-04-26 10:52:31 -0300

    Update Aviao.java

--- src/Models/Aviao.java
+++ src/Models/Aviao.java
@@ -1,26 +1,27 @@
 package Models;
 
 public class Aviao {
-    protected int idAviaoAviao;
-    protected String modeloAviao;
-    protected float tamanhoAviao;
+    
+    private int IdAviaoAviao;
+    private String modeloAviao;
+    private float tamanhoAviao;
 
     public Aviao() {
 
     }
 
-    public Aviao(int idAviaoAviao, String modeloAviao, float tamanhoAviao) {
-        this.idAviaoAviao = idAviaoAviao;
+    public Aviao(int IdAviaoAviao, String modeloAviao, float tamanhoAviao) {
+        this.IdAviaoAviao = IdAviaoAviao;
         this.modeloAviao = modeloAviao;
         this.tamanhoAviao = tamanhoAviao;
     }
 
-    public int getIdAviaoAviao() {
-        return idAviaoAviao;
+    public int getIdAviao() {
+        return IdAviao;
     }
 
-    public void setIdAviaoAviao(int idAviaoAviao) {
-        this.idAviaoAviao = idAviaoAviao;
+    public void setIdAviao(int IdAviao) {
+        this.IdAviao = IdAviao;
     }
 
     public String getModeloAviao() {
