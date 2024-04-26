8814885271fbf7f6e4bea7fe72db89a125b0874c
mafioletti98 <141834882+mafioletti98@users.noreply.github.com>
2024-04-26 10:56:33 -0300

    Update Piloto.java

--- src/Models/Piloto.java
+++ src/Models/Piloto.java
@@ -1,27 +1,27 @@
 package Models;
 
-public class Piloto extends Pessoas {
+public class Piloto extends Pessoa {
     // brevê é o nome dado a certificaçao concedida pra pilotar tipo carteira de
     // piloto 
-    int idbreve;
+    int Idbreve;
     // piloto pode ser piloto (conduz a aeronave) ou copiloto(conduz navegaçao)
     String funcaoVoo;
 
     public Piloto() {
     }
 
-    public Piloto(String nome, int cpf, String telefone, int idbreve, String funcaoVoo) {
+    public Piloto(String nome, int cpf, String telefone, int Idbreve, String funcaoVoo) {
         super(nome, cpf, telefone);
-        this.idbreve = idbreve;
+        this.idbreve = Idbreve;
         this.funcaoVoo = funcaoVoo;
     }
 
     public int getIdbreve() {
-        return idbreve;
+        return Idbreve;
     }
 
-    public void setIdbreve(int idbrevê) {
-        this.idbreve = idbrevê;
+    public void setIdbreve(int Idbreve) {
+        this.Idbreve = Idbrevê;
     }
 
     public String getFuncaoVoo() {
